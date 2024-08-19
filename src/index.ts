c9a9a08dc20f23687f1c620e57dfebc3281dc535
Timothy Cool <tcoolpg@gmail.com>
2024-08-18 17:06:46 -0700

    feat: upgrade Zylem to 0.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
 import { game, stage, sphere } from '@tcool86/zylem';
 
-const example = game({
-	id: 'basic-game',
-	globals: {
-		score: 0
-	},
-	stages: [
-		stage({
-			children: () => [
-				sphere({
-					update: ({ entity: ball, inputs }) => {
-						const { horizontal, vertical } = inputs[0];
-						ball.moveXY(horizontal * 5, -vertical * 5);
-					}
-				})
-			]
-		})
-	],
-});
+const example = game(
+	stage(),
+	sphere({
+		update: ({ entity: ball, inputs }) => {
+			const { horizontal, vertical } = inputs[0];
+			ball.moveXY(horizontal * 5, -vertical * 5);
+		}
+	})
+);
 
 example.start();
