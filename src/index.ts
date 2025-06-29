1e61d71cb3ed576b088d11015305d68f82c074d3
Timothy Cool <tcoolpg@gmail.com>
2025-06-28 21:51:18 -0700

    feature: update basic ball game example

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import { game, stage, sphere } from '@tcool86/zylem';
+import { game, sphere, makeMoveable } from '@tcool86/zylem';
 
-const example = game(
-	stage(),
-	sphere({
-		update: ({ entity: ball, inputs }) => {
-			const { horizontal, vertical } = inputs[0];
-			ball.moveXY(horizontal * 5, -vertical * 5);
-		}
-	})
-);
-
-example.start();
+const ball = await sphere();
+makeMoveable(ball).onUpdate(({ entity, inputs }) => {
+	const { Horizontal, Vertical } = inputs.p1.axes;
+	entity.moveXY(Horizontal.value * 5, -Vertical.value * 5);
+});
+game(ball).start();
